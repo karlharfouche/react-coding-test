819d1d3c4349b5940b3f1c549e8268b80b1dca04
karlharfouche <karlharfouche1@gmail.com>
2021-09-03 14:27:04 +0300

    Stable Version Without Bonuses

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import React from 'react';
 import './App.css';
-//import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './pages/Home'
 import History from './pages/History';
 import Payloads from './pages/Payloads';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { theme } from './theme';
-
-// Tab pannel:
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
+import { Route, BrowserRouter, Switch, Link, Redirect } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -67,26 +30,31 @@ function App() {
     setValue(newValue);
   };
 
+  const routes = ['/home', '/history', '/payloads']
+
   return (
     <>
     <ThemeProvider theme={theme}> 
       <CssBaseline />
-      <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} className={classes.root} aria-label="simple tabs example" variant="fullWidth">
-          <Tab label="Home" {...a11yProps(0)} />
-          <Tab label="History" {...a11yProps(1)} />
-          <Tab label="Payloads" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        <Home />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <History />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Payloads />
-      </TabPanel>
+      <BrowserRouter>
+        <Route path="/">
+          <AppBar position="sticky">
+            <Tabs value={value} onChange={handleChange} className={classes.root} variant="fullWidth">
+              <Tab label="Home" value={routes[0]} component={Link} to={routes[0]} />
+              <Tab label="History" value={routes[1]} component={Link} to={routes[1]} />
+              <Tab label="Payloads" value={routes[2]} component={Link} to={routes[2]} />
+            </Tabs>
+          </AppBar>
+        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home" component={Home} />
+          <Route path="/history" component={History} />
+          <Route path="/payloads" component={Payloads} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider> 
     </>
   );
